e180f6ab6ccc195a8c4ad54d9f528960ed181e41
ouyanglang <1041212092@qq.com>
2018-10-26 14:58:42 +0800

    Initial commit

---
+++ src/main/java/com/startai/servlet/ServletDemo.java
@@ -0,0 +1,21 @@
+package com.startai.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "ServletDemo")
+public class ServletDemo extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request,response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("aaaaa");
+
+        response.getWriter().append("aaaaa");
+    }
+}
